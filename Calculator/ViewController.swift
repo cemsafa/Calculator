aecc0160b5c928ff0a8aaba5dc0e99a1ba18f6a0
Cem Safa <cemsafa@gmail.com>
2020-07-23 17:31:23 +0300

    initial commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Cem Safa on 23.07.2020.
+//  Copyright © 2020 Cem Safa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a non-number button is pressed
+    
+    }
+
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+    
+    }
+
+}
+
